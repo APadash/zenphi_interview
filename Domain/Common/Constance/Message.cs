f64d109878d777d808689e277d1e1a2577ef8c13
APadash <ap.prog1@gmail.com>
2022-11-19 20:13:59 +0330

    init Project

---
+++ Domain/Common/Constance/Message.cs
@@ -0,0 +1,9 @@
+namespace Domain.Common.Helper
+{
+    public static class Message
+    {
+        public const string SuccessfullMessage = "Operation is Successfull";
+        public const string ErrorMessage = "The operation has encountered an error";
+
+    }
+}
---
+++ Domain/Common/Dto/User/UserDto.cs
@@ -0,0 +1,9 @@
+namespace Domain.Common.Dto.User
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
